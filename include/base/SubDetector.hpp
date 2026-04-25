7e4e5c92f0161c97d13b23b9bd077aad048fc148
JungleMan796 <callumpeterwilliamson08@gmail.com>
2026-04-25 14:43:11 +0100

    Created frameworrk for base class SubDetector.

---
+++ include/base/SubDetector.hpp
@@ -0,0 +1,49 @@
+// Callum Williamson.
+// Student ID: 11399397.
+
+// Header, file interface for the SubDetector (base) class.
+
+#ifndef SUBDETECTOR_HPP
+#define SUBDETECTOR_HPP
+
+#include "base/Particle.hpp"
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+class SubDetector
+{
+    protected:
+    std::string sd_name; // Name of SubDetector (sd).
+
+    bool status; // true = on, false = off
+
+    public:
+    //Defualt constructor.
+    SubDetector();
+
+    // Parameterised constructor.
+    SubDetector(const std::string sd_name, bool status);
+
+    // Destructor.
+    virtual ~SubDetector();
+
+    // Getters.
+    std::string get_sd_name() const;
+
+    bool get_status() const;
+
+    // Setters.
+    void set_sd_name(const std::string& new_sd_name);
+
+    void set_status(bool new_status);
+
+    // Pure virtual, print charactertistics common to all SubDetectors.
+    virtual void print_info() const = 0;
+
+    // Print virtual function for detection of Particle class.
+    virtual void detect(const Particle& particle) const = 0;
+};
+
+#endif
---
+++ src/base/SubDetector.cpp
@@ -0,0 +1,73 @@
+// Callum Williamson.
+// Student ID: 11399397.
+
+// Implementation file for the SubDetector class.
+
+#include "base/SubDetector.hpp"
+#include "core/Debug.hpp"
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+// Defualt Constructor.
+SubDetector::SubDetector() : sd_name("Defualt SubDetector name"), status(false)
+{
+    if (Debug::show_messages)
+    {
+        std::cout << "Calling SubDetector Defualt Constructor" << std::endl;
+    }
+}
+
+// Parameterised Constructor.
+SubDetector::SubDetector(std::string sd_name, bool status)
+    :sd_name(sd_name), status(status) // Will adjust for validation.
+{
+    if (Debug::show_messages)
+    {
+        std::cout << "Calling SubDetector Parameterised Constructor." << std::endl;
+    }
+}
+
+// Destructor.
+SubDetector::~SubDetector()
+{
+    if (Debug::show_messages)
+    {
+        std::cout << "Calling SubDetector Destructor." << std::endl;
+    }
+}
+
+// Getters.
+std::string SubDetector::get_sd_name() const
+{
+    return sd_name;
+}
+
+bool SubDetector::get_status() const
+{
+    return status;
+}
+
+// Setters.
+void SubDetector::set_sd_name(const std::string& new_sd_name)
+{
+    sd_name = new_sd_name; // Will adjust for validation.
+}
+
+void SubDetector::set_status(bool new_status)
+{
+    status = new_status;
+}
+
+// Print characteristics common to all SubDetectors.
+void SubDetector::print_info() const
+{
+    // Print sd_name.
+    std::cout << "Sub-detector name: " << sd_name << std::endl;
+
+    // Print status.
+    std::cout << "Status (0=off/1=on): " << status << std::endl;
+}
+
+// No detect called as it is derived SubDetector specific.
