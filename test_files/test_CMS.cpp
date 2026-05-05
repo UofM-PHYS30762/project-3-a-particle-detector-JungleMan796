e7979d355c2173dd936d436aeadcd94c27301616
JungleMan796 <callumpeterwilliamson08@gmail.com>
2026-05-05 16:06:27 +0100

    Trimming back test_CMS.cpp, this will be used in my main.cpp

--- test_files/test_CMS.cpp
+++ test_files/test_CMS.cpp
@@ -12,6 +12,10 @@
 #include "derived/Particle/Electron.hpp"
 #include "derived/Particle/Muon.hpp"
 #include "derived/Particle/Photon.hpp"
+#include "derived/Particle/Pion.hpp"
+#include "derived/Particle/Kaon.hpp"
+#include "derived/Particle/Proton.hpp"
+#include "derived/Particle/Neutron.hpp"
 
 #include "derived/SubDetector/HadronCalorimeter.hpp"
 #include "derived/SubDetector/EMCalorimeter.hpp"
@@ -34,25 +38,25 @@ int main()
     det_sys_CMS.add_sub_detector(std::make_unique<MuonChamber>("Muon Chamber", true));
 
     // Print CMS configuration.
-    det_sys_CMS.print_configuration();
+    // det_sys_CMS.print_configuration();
 
     // Define particle for detection test.
     Electron electron_1(10.0, 1.0, 2.0, 3.0, false, 0.0);
-    electron_1.print_info();
-    electron_1.set_is_anti(true);
-    electron_1.print_info();
+    // electron_1.print_info();
+    // electron_1.set_is_anti(true);
+    // electron_1.print_info();
 
     Muon muon_1(20.0, 4.0, 5.0, 6.0, false, 0.0);
-    muon_1.print_info();
-    muon_1.set_is_anti(true);
-    muon_1.print_info();
+    // muon_1.print_info();
+    // muon_1.set_is_anti(true);
+    // muon_1.print_info();
 
     Photon photon_1(30.0, 7.0, 9.0, 11.0, false);
-    photon_1.print_info();
+    // photon_1.print_info();
 
     // Detection.
-    det_sys_CMS.detect(electron_1);
-    det_sys_CMS.detect(muon_1);
+    // det_sys_CMS.detect(electron_1);
+    // det_sys_CMS.detect(muon_1);
     det_sys_CMS.detect(photon_1);
 
     
