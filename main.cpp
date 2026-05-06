71e75e65e9910f0147097e6bf645022556a79088
JungleMan796 <callumpeterwilliamson08@gmail.com>
2026-05-06 13:29:22 +0100

    Setting out the main to demonstrate the main features of the final project.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// main.cpp for the final project - particle detector.
+// Callum Williamson.
+// Student ID: 11399397.
+
+// General includes.
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Header includes.
+#include "core/DetectorSystem.hpp"
+
+#include "derived/Particle/Electron.hpp"
+#include "derived/Particle/Muon.hpp"
+#include "derived/Particle/Photon.hpp"
+#include "derived/Particle/Pion.hpp"
+#include "derived/Particle/Kaon.hpp"
+#include "derived/Particle/Proton.hpp"
+#include "derived/Particle/Neutron.hpp"
+
+#include "derived/SubDetector/HadronCalorimeter.hpp"
+#include "derived/SubDetector/EMCalorimeter.hpp"
+#include "derived/SubDetector/MuonChamber.hpp"
+#include "derived/SubDetector/Tracker.hpp"
+
+int main()
+{
+    return 0;
+}
