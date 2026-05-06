3fe3a22cd9d33dbfb711e3bdf5d20a0e7593633a
JungleMan796 <callumpeterwilliamson08@gmail.com>
2026-05-06 13:53:22 +0100

    Created the CMS DetectorSystem.

--- main.cpp
+++ main.cpp
@@ -27,9 +27,20 @@
 int main()
 {
     // Demonstration of minimum functionality.
-
+    std::cout << "--- Demonstration of minimum fucntionality in main.cpp ---" << std::endl;
+    std::cout << std::endl; // Line break.
+    std::cout << "Creating the CMS DetectorSystem with all the SubDetector components." << std::endl;
+    std::cout << std::endl; // Line break.
+    // As DSMessages are currently on you will see the print statements of the addtion of the SubDetectors.
+    
     // Creation of arbitrary detector (DetectorSystem) with sub-detector (SubDetector) components.
-    DetectorSystem det_sys_CMS("CMS Detector System");
+    DetectorSystem det_sys_CMS("CMS Detector System"); // Will construct CMS detector with all components (SubDetectors).
+
+    // CMS SubDetectors will be added below in correct physical order.  
+    det_sys_CMS.add_sub_detector(std::make_unique<Tracker>("Silicon Tracker", true));
+    det_sys_CMS.add_sub_detector(std::make_unique<EMCalorimeter>("Crystal Electromagnetic Calorimeter", true));
+    det_sys_CMS.add_sub_detector(std::make_unique<HadronCalorimeter>("Hadron Calorimeter", true));
+    det_sys_CMS.add_sub_detector(std::make_unique<MuonChamber>("Muon Chamber", false)); // MuonChamber is off for testing purposes.
 
     return 0;
 }
